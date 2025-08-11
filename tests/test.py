df39e23a4ca71ef04c6b322ac400d4fcf1541990
Inoland <contact@nobandegan.com>
2025-08-11 23:30:04 +0400

    update tests

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,69 @@
 import asyncio
 
-from src.CloudreveClient import CloudreveClient
-from src.CloudreveClient.utils import save_url_as_file, read_file_as_bytes
-
+from src.inocloudreve import CloudreveClient
 
 async def main():
-    client = CloudreveClient("", "", "")
+    client = CloudreveClient()
+
+    client.init(
+        base_url= ""
+    )
+
     ping_result = await client.ping()
     print("Ping response:", ping_result)
 
-    password_login_result = await client.password_sign_in()
+    password_login_result = await client.password_sign_in(
+        email="",
+        password=""
+    )
     print("password_login_result:", password_login_result)
+    print("is_token_valid:", client.is_token_valid())
 
-    print("is_token_valid:",client.is_token_valid())
+    download_file_result = await client.download_file(
+        path_to_save=r"",
+        uri="",
+        archive=False,
+        no_cache=True,
+    )
+    print("download_file_result:", download_file_result)
+
+    #upload_file_result = await client.upload_file(
+    #    local_path=r"",
+    #    remote_path=f"",
+    #    storage_policy=""
+    #)
+    #print("upload_file_result:", upload_file_result)
+
+    #generate_token_result = client.generate_token(
+    #    master_key=master_key,
+    #    user_id=""
+    #)
+    #print("generate_token_result:", generate_token_result)
+    #print("is_token_valid:", client.is_token_valid())
+
+    #decode_token_result = client.decode_token(
+    #    master_key=master_key,
+    #    access_token=generate_token_result["token"]["access_token"]
+    #)
+    #print("decode_token_result:", decode_token_result)
+
+    #print(password_login_result["token"]["access_token"])
+
+    #decode_token2_result = client.decode_token(
+    #    master_key=master_key,
+    #    access_token=password_login_result["token"]["access_token"]
+    #)
+    #print("decode_token_result:", decode_token2_result)
+
+    #get_last_folder_result = await client.get_last_folder_or_file(uri="")
+    #print("get_last_folder_result:", get_last_folder_result["data"])
 
     #refresh_token_result = await client.refresh_token()
     #print ("refresh_token_result:",refresh_token_result)
 
-    #example_file_path = "Spark/test"
-    #example_file_path = "Spark/test/input/TanaRain_073.png"
+    #example_file_path = ""
+    #example_file_path = ""
+    #example_file_path = ""
 
     #result = await client.get_download_url(example_file_path, True)
     #print("result:", result)
@@ -26,13 +71,13 @@ async def main():
 
     #save_url_as_file_result = await client.save_url_as_file(
     #    result["url"],
-    #    r"E:\NIL\spark\python\InoGenie\assets\example batch",
+    #    r"",
     #    result["file_name"],
     #    result["extension"])
     #print("save_url_as_file_result:", save_url_as_file_result)
 
     #get_file_info_result = await client.get_file_info(example_file_path, True)
-    #get_file_info_result = await client.get_file_info("", "1752", True, True)
+    #get_file_info_result = await client.get_file_info("", "", True, True)
     #print("get_file_info_result:", get_file_info_result)
 
     #create_download_url_result = await client.create_download_url(
@@ -41,9 +86,9 @@ async def main():
     #print("create_download_url_result:", create_download_url_result)
 
 
-    #read_file_as_bytes = client.read_file_as_bytes(r"E:\NIL\spark\python\InoGenie\assets\AlyTay\AlyTay_00001.png")
+    #read_file_as_bytes = client.read_file_as_bytes(r"")
     #update_file_content_result = await client.update_file_content(
-    #    file_uri="Spark/test/AlyTay_00004.png",
+    #    file_uri="",
     #    content=read_file_as_bytes["data"]
     #)
 
@@ -52,7 +97,16 @@ async def main():
     #list_files_result = await client.list_files("")
     #print("list_files_result:", list_files_result)
 
+    #create_upload_session_result = await client.create_upload_session("", , "")
+    #print("create_upload_session_result:", create_upload_session_result)
 
+    #delete_upload_session_result = await client.delete_upload_session(
+    #    "",
+    #    "")
+    #print("delete_upload_session_result:", delete_upload_session_result)
+
+    #delete_file_result = await client.delete_file([""])
+    #print("delete_file_result:", delete_file_result)
 
     await client.close()
 
